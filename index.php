8b261bbcb8ed66ab255e3c7a788f79a9f2b6d2a3
Felipe Romao <feliperomao.a@gmail.com>
2018-02-17 20:07:17 -0200

    update in the class index

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE>
+<html lang="pt-BR">
+    <head>
+        <meta charset="utf-8" />    
+        <title>PHP with POO</title>
+    </head>
+    <body>
+         <?php 
+            echo "Olá feioso =D";
+         ?>
+    </body>
+</html>
+
